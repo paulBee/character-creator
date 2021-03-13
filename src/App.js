544abc8bbf10346224923f7c89ed4c6a90b68900
Paul B <paul.beecroft@gmail.com>
2021-03-13 15:43:39 +0000

    adding styles to meet criteria in readme

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
     <>
+      <Backdrop />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Backdrop/index.jsx
@@ -0,0 +1,5 @@
+import styles from './Backdrop.module.css'
+
+export default function Backdrop() {
+  return <div className={styles.backdrop}/>
+}
